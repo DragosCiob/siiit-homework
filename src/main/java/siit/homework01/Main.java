efd7afa8994a7ed69e721824b635af18659cbeb6
DragosCiob <ciobanudragos80@gmail.com>
2022-06-22 20:19:43 +0300

    checking ok

--- src/main/java/siit/homework01/Main.java
+++ src/main/java/siit/homework01/Main.java
@@ -3,5 +3,6 @@ package siit.homework01;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Guys");
+        System.out.println("what's going on?");
     }
 }
